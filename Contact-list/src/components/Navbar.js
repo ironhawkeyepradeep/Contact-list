b5024b9def61cdc5722c69a00c8527bfd07899c7
Pradeep Singh Yadav <137269372+ironhawkeyepradeep@users.noreply.github.com>
2023-08-31 16:07:13 +0530

    change

--- Contact-list/src/components/Navbar.js
+++ Contact-list/src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <nav className='navbar navbar-expand-lg navbar-dark bg-dark py-2'>
             <div className='container-fluid'>
-                <Link to='/' className='navbar-brand ml-5'>Contact List App by Pradeep.</Link>
+                <Link to='/' className='navbar-brand ml-5'>Contact List by Pradeep</Link>
             </div>
         </nav >
     )
